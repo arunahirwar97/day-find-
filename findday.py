b69c7507f2aceb2961b67b575876692307d285b0
websiteaap <53229705+websiteaap@users.noreply.github.com>
2019-10-31 23:26:20 +0530

    Add files via upload

---
+++ findday.py
@@ -0,0 +1,18 @@
+print("\t\tFIND ANY DAY ")
+x = int(input("Please Enter any day day number 1 - 7 :"))
+
+if x==1 :
+    print("SUNDAY ")
+elif x==2 :
+    print("Monday")
+elif x==3 :
+    print("Tuesday")
+elif x==4 :
+    print("Wednesday")
+elif x==5 :
+    print("Thursday")
+elif x==6 :
+    print("Friday")
+elif x==7 :
+    print("Suterday")
+else:print("Wrong enter day number ")
